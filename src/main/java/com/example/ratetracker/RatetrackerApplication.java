261446de3fd29847d06fca4a75cfdd9bc4f76e3f
Pavlo <pppp_84@ukr.net>
2025-05-23 18:51:13 +0300

    first commit,created springboot project

---
+++ src/main/java/com/example/ratetracker/RatetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ratetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatetrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ratetracker/RatetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ratetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

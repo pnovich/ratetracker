b007f20b09d7e0635a57aac20f027d7648b21e17
Pavlo <pppp_84@ukr.net>
2025-05-25 03:55:25 +0300

    swagger addad

--- src/main/java/com/example/ratetracker/AppConfig.java
+++ src/main/java/com/example/ratetracker/AppConfig.java
@@ -1,7 +1,9 @@
 package com.example.ratetracker;
 
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -9,6 +11,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RateTracker API")
+                        .version("1.0")
+                        .description("API for managing exchange rates"));
     }
 
 }
